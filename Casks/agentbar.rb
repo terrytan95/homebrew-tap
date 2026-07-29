c92619b16edda016ab3f9c44658fac105b8bed0b
Terry Tan <tmy1995hflc@gmail.com>
2026-07-28 19:37:04 -0700

    feat(agentbar): update to v2.4.6

--- Casks/agentbar.rb
+++ Casks/agentbar.rb
@@ -1,6 +1,6 @@
 cask "agentbar" do
-  version "2.4.5"
-  sha256 "ed9c793c2cf08a6db915f3b594423153fac7608d7818b9758cd816a634f04f31"
+  version "2.4.6"
+  sha256 "a2b18969e7d66386612b0e1db14924a3e5448fa697cdd023f05c0eaad7c50930"
 
   url "https://github.com/terrytan95/AgentBar/releases/download/v#{version}/AgentBar-v#{version}.zip",
       verified: "github.com/terrytan95/AgentBar/"
